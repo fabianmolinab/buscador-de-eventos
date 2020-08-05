bc7918c506631843a5f491746175fe038291dafd
fabianmolinab <molinafabian11b@gmail.com>
2020-08-05 15:54:35 -0500

    Vamos avanzado

---
+++ js/eventbrite.js
@@ -0,0 +1,11 @@
+class EventBrite {
+  constructor() {
+    this.token_auth = "SL72LNKMB7BPAPR3E4K2";
+    this.ordenar = "date";
+  }
+
+  // Obtiene las categorias en init()
+  async obtenerCategorias() {
+    // Consultar la REST API
+  }
+}
---
+++ js/interfaz.js
@@ -0,0 +1,17 @@
+class Interfaz {
+  constructor () {
+    // Inicializa la app al instanciar
+    this.init();
+    // Leer el resultado
+    this.listado = document.getElementById('resultado-eventos');
+  }
+
+  //Metodo para cuando inicialice la app
+  init() {
+
+  }
+  // Impirmir categorias
+  imprimirCategorias() {
+    const listaCategorias = eventbrite.obtenerCategorias();
+  }
+}
