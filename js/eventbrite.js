9bf64073d6f215694d39c65b1eb9069c3889e939
fabianmolinab <molinafabian11b@gmail.com>
2020-08-05 19:01:34 -0500

    Ingresamos la base de datos al formulario pero tiene errores

--- js/eventbrite.js
+++ js/eventbrite.js
@@ -7,5 +7,14 @@ class EventBrite {
   // Obtiene las categorias en init()
   async obtenerCategorias() {
     // Consultar la REST API
+    const respuestaCategorias = await fetch(
+      `https://www.eventbriteapi.com/v3/categories/?token=${this.token_auth}`
+    );
+
+    //Resperar la respuesta de las categrias y devolver un JSON
+    const categorias = await respuestaCategorias.json();
+
+    // Devolvemos el resultado
+    return categorias;
   }
 }
